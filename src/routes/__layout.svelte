3df29e19537921b6d6f02f18e0f6b135ff92c89b
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-07-26 12:11:10 +0530

    Remove Footer import (build issue)

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,6 @@
 <script>
 	import '../global.css';
 	import Header from '$lib/Header.svelte'
-	import Footer from '$lib/Footer.svelte'
 	import AlertList from '$lib/alert/AlertList.svelte'
 </script>
 
@@ -10,4 +9,3 @@
 <main class="w-full h-screen mx-auto relative px-2 sm:px-6 lg:px-8 bg-gray-100">
 	<slot />
 </main>
-<Footer/>
