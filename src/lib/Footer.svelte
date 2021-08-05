705e93f3e1d74b115124eb6850849f18631fee06
Aftab Alam <532906+one-aalam@users.noreply.github.com>
2021-08-05 07:22:30 +0530

    Update TS Config

--- src/lib/Footer.svelte
+++
@@ -1,3 +0,0 @@
-<footer class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-    <slot/>
-</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -34,17 +34,6 @@
 {/if}
 
 <style style lang="postcss">
-	:root {
-		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
-			'Open Sans', 'Helvetica Neue', sans-serif;
-	}
-
-	main {
-		@apply text-center;
-		@apply p-4;
-		@apply mx-auto;
-	}
-
 	h1 {
 		@apply text-red-600;
 		@apply text-6xl;
